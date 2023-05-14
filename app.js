c2f72465449f91c53a0cc749f78186602cd991d3
Azhar-Siddiq <azharsiddiq73@gmail.com>
2023-05-14 11:39:10 +0500

    To-Do_Application

---
+++ app.js
@@ -0,0 +1,37 @@
+var inputBox = document.getElementById('inputBox');
+var listContainer = document.getElementById('listContainer');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You must write something!');
+    }
+    else{
+        var li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        var span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
